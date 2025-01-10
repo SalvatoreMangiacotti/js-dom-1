2aebe1afaefb2448055836c265470fe5310c1498
Salvatore <workstudioms@gmail.com>
2025-01-10 17:06:24 +0100

    js-dom-1 exercise, complete

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("Hello World");
+const whiteLamp = document.querySelector('.white-lamp');
+const yellowLamp = document.querySelector('.yellow-lamp');
+const offOnButton = document.getElementById('button');
+
+console.log(whiteLamp, yellowLamp, offOnButton);
+
+// Risoluzione esercizio 1 //
+offOnButton.addEventListener('click', () => {
+
+    whiteLamp.className = 'hidden';
+    yellowLamp.className = 'active';
+
+})
+
+
+
+// Risoluzione esercizio 2 //
+
+
+// offOnButton.addEventListener('click', () => {
+
+//     whiteLamp.classList.add('hidden');
+//     yellowLamp.classList.remove('hidden');
+    
+// })
